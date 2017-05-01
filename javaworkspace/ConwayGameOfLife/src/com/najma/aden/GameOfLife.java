ac37aa0b23a5fe718d2ea972440476e3b4dd2d15
najma103 <najma.aden@gmail.com>
2017-05-01 17:29:21 -0400

    initial commit

---
+++ javaworkspace/ConwayGameOfLife/src/com/najma/aden/GameOfLife.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.najma.aden;
+
+/**
+ * @author najmaaden
+ *
+ */
+public class GameOfLife {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
